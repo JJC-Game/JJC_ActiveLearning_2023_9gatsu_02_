32be7d10a0d94a4eb50444e74c656d218e37c7d0
junya95191 <x49861656@gmail.com>
2023-09-14 10:24:14 +0900

    web.php書き換え

--- Etc/Laravel/itemgatya/web.php
+++ Etc/Laravel/itemgatya/web.php
@@ -1,4 +1,5 @@
-<?php
+<?php Route::get('/get_items/{id}', [AppUserController::class, 'app_user'])->name('al.app_user');
+
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppUserController;
